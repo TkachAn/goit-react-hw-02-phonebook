296c4d37cd5791773872a1009bedf1030d3ba228
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-15 09:35:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,79 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+const Gender = {
+  MALE: 'male',
+  FEMALE: 'female',
 };
+
+const INITIAL_STATE = {
+  login: '',
+  email: '',
+  password: '',
+  agreed: false,
+  gender: null,
+  age: '',
+};
+export class App extends Component {
+  state = {
+    ...INITIAL_STATE,
+  };
+
+  handleChange = evt => {
+    const { name, value, type, checked } = evt.target;
+    // Если тип элемента checkbox, берем значение checked,
+    // в противном случае value
+    this.setState({ [name]: type === 'checkbox' ? checked : value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { login, email, password, agreed } = this.state;
+    console.log(
+      `Login: ${login}, Email: ${email}, Password: ${password}, Agreed: ${agreed}`
+    );
+
+    /* ... */
+  };
+
+  render() {
+    const { login, email, password, agreed, gender, age } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {/* ... */}
+
+        <label>
+          Choose your age
+          <select name="age" value={age} onChange={this.handleChange}>
+            <option value="" disabled>
+              ...
+            </option>
+            <option value="18-25">18-25</option>
+            <option value="26-35">26-35</option>
+            <option value="36+">36+</option>
+          </select>
+        </label>
+
+        <button type="submit" disabled={!agreed}>
+          Sign up as {login}
+        </button>
+      </form>
+    );
+  }
+}
+// export class App extends Component {
+//   state = {
+//     contacts: [],
+//     name: '',
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <p>форма добавления контактов</p>
+//         <p>список контактов</p>
+//         <p>элемент списка контактов</p>
+//         <p>фильтр поиска</p>
+//       </div>
+//     );
+//   }
+// }
