d545c7ac80c4d2e36cb12c3a624130f23448dec7
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-15 23:40:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from './App';
 import './index.css';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+// import React from 'react';
+// import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
