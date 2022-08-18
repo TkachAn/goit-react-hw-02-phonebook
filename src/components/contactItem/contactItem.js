96d463333f01539fc44cda96bb849483a5d4d21e
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-18 12:39:40 +0300

    Create contactItem.js

---
+++ src/components/contactItem/contactItem.js
@@ -0,0 +1,11 @@
+// import {typesProps} from ''
+// import css from './contactItem.module.css';
+
+function ContactItem({ name, tel }) {
+  return (
+    <>
+      {name}: {tel}
+    </>
+  );
+}
+export default ContactItem;
