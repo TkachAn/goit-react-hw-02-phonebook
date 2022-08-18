5bb88ba99c75ae4fc02ad8010791f8cb87031d63
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-18 12:39:52 +0300

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
-
+import ContactItem from '../contactItem/contactItem';
 function ContactList({ contacts, onDeleteContact }) {
   return (
     <ul className={css.list}>
       {contacts.map(({ id, name, number }) => (
         <li className={css.item} key={id}>
           <p className={css.info}>
-            {name}: {number}
+            <ContactItem name={name} tel={number} />
           </p>
           <button
             className={css.btn}
