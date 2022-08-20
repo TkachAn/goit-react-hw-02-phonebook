81c1440e7c5f99a5a5d5be58bbb97e373e640dbc
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-20 16:14:21 +0300

    Update contactItem.js

--- src/components/contactItem/contactItem.js
+++ src/components/contactItem/contactItem.js
@@ -1,11 +1,26 @@
-// import {typesProps} from ''
-// import css from './contactItem.module.css';
+import PropTypes from 'prop-types';
+import css from './contactItem.module.css';
 
-function ContactItem({ name, tel }) {
+function ContactItem({ user, tel, onDel, delId }) {
   return (
-    <>
-      {name}: {tel}
-    </>
+    <div className={css.info}>
+      {user}: {tel}
+      <button
+        className={css.btn}
+        type="button"
+        onClick={() => {
+          onDel(delId);
+        }}
+      >
+        X
+      </button>
+    </div>
   );
 }
+ContactItem.propTypes = {
+  delId: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired,
+  tel: PropTypes.string.isRequired,
+  onDel: PropTypes.func.isRequired,
+};
 export default ContactItem;
